f5e10475da243e3e7edfec8f4f17aba00d1f8a72
“SahilKharya” <kharyasahil@gmail.com>
2020-02-11 17:00:47 +0530

    add changes

--- src/main/java/in/pune/royforge/eledgerapi/EledgerapiApplication.java
+++ src/main/java/in/pune/royforge/eledgerapi/EledgerapiApplication.java
@@ -3,6 +3,13 @@ package in.pune.royforge.eledgerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author pradyroy
+ *
+ */
+
+
 @SpringBootApplication
 public class EledgerapiApplication {
 
