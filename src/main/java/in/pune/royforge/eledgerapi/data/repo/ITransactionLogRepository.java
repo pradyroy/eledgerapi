81b44ba1166ffe2087ed7b634bf61d4f8bcd4ac6
kharya1996 <kharyashashank02@gmail.com>
2020-02-18 15:37:28 +0530

    Change the method name findByLenderId to transactionsByLenderId

--- src/main/java/in/pune/royforge/eledgerapi/data/repo/ITransactionLogRepository.java
+++ src/main/java/in/pune/royforge/eledgerapi/data/repo/ITransactionLogRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import in.pune.royforge.eledgerapi.data.entity.TransactionEntity;
 
 public interface ITransactionLogRepository extends PagingAndSortingRepository<TransactionEntity, String> {
+	public Iterable<TransactionEntity> findByLenderId(String lenderId);
 
 }
