d254204e4827bea847a2cf2744a6560a3ba16ed9
“Arpit <arpitgoyal437@gmail.com>
2020-02-20 12:09:11 +0530

    custom exception handler

--- src/main/java/in/pune/royforge/eledgerapi/data/service/WalletServiceImpl.java
+++ src/main/java/in/pune/royforge/eledgerapi/data/service/WalletServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import in.pune.royforge.eledgerapi.data.dao.IWalletDAO;
 import in.pune.royforge.eledgerapi.data.model.WalletData;
 import in.pune.royforge.eledgerapi.data.model.WalletTransaction;
+import in.pune.royforge.eledgerapi.exceptionhandler.RecordNotFoundException;
 
 @Service
 public class WalletServiceImpl implements WalletService {
@@ -20,8 +21,12 @@ public class WalletServiceImpl implements WalletService {
 	}
 
 	@Override
-	public WalletData getWallet(Long walletId) {
-		return walletEntityDAO.getWallet(walletId);
+	public WalletData getWallet(Long walletId) throws RecordNotFoundException{
+		WalletData walletData = walletEntityDAO.getWallet(walletId);
+		if(walletData == null) {
+			throw new RecordNotFoundException("Record Not Found");
+		}
+		return walletData;
 	}
 
 	@Override
---
+++ src/main/java/in/pune/royforge/eledgerapi/exceptionhandler/CustomExceptionHandler.java
@@ -0,0 +1,26 @@
+package in.pune.royforge.eledgerapi.exceptionhandler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+	@ExceptionHandler(RecordNotFoundException.class)
+	public final ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException ex) {
+		Date currentDate = new Date();
+		ErrorResponse error = new ErrorResponse(currentDate, ex.getMessage(), HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Object>(error, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex) {
+		Date currentDate = new Date();
+		ErrorResponse error = new ErrorResponse(currentDate, ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<Object>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
