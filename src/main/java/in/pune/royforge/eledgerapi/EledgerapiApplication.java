3f307b529f351f62c1ca04b04ede0e7a21c0c3cb
“SahilKharya” <kharyasahil@gmail.com>
2020-02-21 16:21:05 +0530

    Starting with swagger in the project

--- src/main/java/in/pune/royforge/eledgerapi/EledgerapiApplication.java
+++ src/main/java/in/pune/royforge/eledgerapi/EledgerapiApplication.java
@@ -2,6 +2,13 @@ package in.pune.royforge.eledgerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 
@@ -10,10 +17,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableSwagger2
 public class EledgerapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EledgerapiApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.any())
+				.build();
+	}
 }
