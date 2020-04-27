457bfc71e5b12914870c7a4916fbfc26b68cb6c9
“SahilKharya” <kharyasahil@gmail.com>
2020-04-27 20:30:46 +0530

    Built project to a multi module maven peoject

--- bddTest/src/test/java/eledgerapi/cucumber/serenity/TransactionImpl.java
+++ bddTest/src/test/java/eledgerapi/cucumber/serenity/TransactionImpl.java
@@ -11,20 +11,17 @@ public class TransactionImpl {
 				.get("http://localhost:8080/transaction/lenderId/{lenderId}/date/{date}").then().statusCode(200);
 	}
 
-	
 	public void getTransactionsList(String string) {
 		SerenityRest.given().contentType(ContentType.JSON).when().get("http://localhost:8080/transaction/" + string)
 				.then().statusCode(200);
 	}
 
-	
 	public void getTransactionsListByLenderId(String path, String lenderId) {
 		SerenityRest.given().contentType(ContentType.JSON).with().pathParam("path", path).with()
 				.pathParam("lenderId", lenderId).when().get("http://localhost:8080/transaction/{path}/{lenderId}")
 				.then().statusCode(200);
 	}
 
-	
 	public void getTransactionsListByLenderIdAndBorrowId(String path, String lenderId, String path2, String borrowId) {
 		SerenityRest.given().contentType(ContentType.JSON).with().pathParam("path", path).with()
 				.pathParam("lenderId", lenderId).with().pathParam("path2", path2).with().pathParam("borrowId", borrowId)
@@ -33,11 +30,12 @@ public class TransactionImpl {
 
 	}
 
-	
 	public void transactionsByLenderIdBetweenDates(String lenderId, String startDate, String endDate) {
 
-		SerenityRest.rest().given().with().pathParam("lenderId", lenderId).with().pathParam("startDate", startDate).with().pathParam("endDate", endDate).when()
-				.get("http://localhost:8080/transaction/lenderId/{lenderId}/startDate/{startDate}/endDate/{endDate}").then().statusCode(200);
-	
+		SerenityRest.rest().given().with().pathParam("lenderId", lenderId).with().pathParam("startDate", startDate)
+				.with().pathParam("endDate", endDate).when()
+				.get("http://localhost:8080/transaction/lenderId/{lenderId}/startDate/{startDate}/endDate/{endDate}")
+				.then().statusCode(200);
+
 	}
 }
