a96069763db290928e79122e71312ef48adab2f2
“SahilKharya” <kharyasahil@gmail.com>
2020-05-22 14:54:50 +0530

    Added Global CORS for UI

--- ConnectionService/src/main/java/in/pune/royforge/connection/ConnectionApp.java
+++ ConnectionService/src/main/java/in/pune/royforge/connection/ConnectionApp.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -30,5 +32,15 @@ public class ConnectionApp {
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
 				.apis(RequestHandlerSelectors.any()).build();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 }
